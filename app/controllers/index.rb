6a51844a5aef0bcd93177645a4d95c91c99fb9ee
[01;31m[K"name"[m[K: "Dylan Yang <[01;31m[K"email"[m[K: null>
2017-07-14 00:08:48 -0400

    Display information

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -3,10 +3,9 @@ get '/' do
 end
 
 post '/' do
-  @image_url = params[:fileToUpload]
-  if sad?(@image_url)
-    erb :'/sad'
-  else
-    erb :'/happy'
-  end
+  email = params[:email]
+  @person = fullcontact_profile(email)
+  @photos = fullcontact_photos(@person)
+  # @attributes = face_attr(@photos.first)
+  erb :'/show'
 end
--- app/helpers/api_parser.rb
+++ app/helpers/api_parser.rb
@@ -1,5 +1,9 @@
+Dotenv.load
+FullContact.configure do |config|
+  config.api_key = ENV['FULL_CONTACT_KEY']
+end
+
 def face_attr(image_url)
-  Dotenv.load
   Unirest.timeout(100)
 
   response = Unirest.get "https://faceplusplus-faceplusplus.p.mashape.com/detection/detect?attribute=glass%2Cpose%2Cgender%2Cage%2Crace%2Csmiling&url=" + image_url,
@@ -8,7 +12,7 @@ def face_attr(image_url)
       "Accept" => "application/json"
     }
 
-  if response.code == 20
+  if response.code == 200
     response_hash = JSON.parse(response.raw_body)
 
     # Hash of attributes(keys) and values
@@ -16,19 +20,18 @@ def face_attr(image_url)
   end
 end
 
-def fullcontact(email)
-  Dotenv.load
-  FullContact.configure do |config|
-    config.api_key = ENV['FULL_CONTACT_KEY']
-  end
+def fullcontact_profile(email)
   # person object is a Hashie and has keys of status, request_id, likelihood, photos, contact_info, organizations, demographics, social_profiles
-  person = FullContact.person(email: 'email')
+  return person = FullContact.person(email: email)
+end
 
+def fullcontact_photos(person)
   if person.status == 200
-    # Array of photos urls
+    # Array of photo urls
     photos = []
     person.photos.each do |info|
       photos.push(info.url)
     end
+    return photos
   end
 end
