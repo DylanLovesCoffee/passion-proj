063d350ba279abe5927334f04d4795bf1ac4ab73
Dylan Yang <email": null>
2017-07-14 13:05:45 -0400

    AJAX for search

--- public/js/application.js
+++ public/js/application.js
@@ -1,8 +1,21 @@
 $(document).ready(function() {
 
-  $(".formWrapper").on("submit", "#emailSearch", function(){
-    $(".formWrapper").append('<div class="loader"></div>')
-    setTimeout(function(){console.log('countdown start')}, 9000);
+  $(".formWrapper").on("submit", "#emailSearch", function(e){
+    e.preventDefault();
+    $(".formWrapper").fadeOut();
+    $("#page-wrapper").append('<div class="loader"></div>')
+    setTimeout(function(){
+      console.log('countdown start');
+    }, 9000);
+    var $form = $(this)
+    $.ajax({
+      url: $form.attr('action'),
+      method: $form.attr('method'),
+      data: $form.serialize()
+    }).done(function(data){
+      $("#page-wrapper").empty();
+      $("#page-wrapper").append(data);
+    });
   });
 
 });
