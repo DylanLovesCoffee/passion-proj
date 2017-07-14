0d96d9b653303f96bf83e5924d29b7e866c6be20
Dylan Yang <email": null>
2017-07-14 13:49:23 -0400

    Edit about page.

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -5,7 +5,6 @@ end
 post '/' do
   email = params[:email]
   @person = fullcontact_profile(email)
-  if request.xhr?
     if @person
       @photos = fullcontact_photos(@person)
       if @photos
@@ -13,11 +12,14 @@ post '/' do
       else
         @attributes = nil
       end
-      erb :'/show', layout: false, locals: { person: @person, photos: @photos, attributes: @attributes }
+      if request.xhr?
+        erb :'/show', layout: false, locals: { person: @person, photos: @photos, attributes: @attributes }
+      else
+        erb :'/show'
+      end
     else
       erb :'/404'
     end
-  end
 end
 
 get '/about' do
